137cb54d7b2c12a246fe7dc38f9dd931732324f0
tenten0213 <takehito.0213@gmail.com>
2014-06-05 12:17:32 +0900

    show members

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+function PopupCtrl($scope) {
+  // TODO: スクレイピングしてデータを取得するように変更する
+  $http.get('url').success(data) {
+    console.log(data);
+  }
+  $scope.members = [
+    {name: 'てんてん', extNumber: '84-37750', status: '在席中'},
+    {name: 'うがちゃん', extNumber: '84-11111', status: '離席中'},
+    {name: 'わだ', extNumber: '84-22222', status: '休み'}
+  ];
+}

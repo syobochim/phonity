957e93e5b85322a147c1c82b71be4ba123f6fac0
tenten0213 <takehito.0213@gmail.com>
2014-06-05 12:59:47 +0900

    get member from terra

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,10 +1,11 @@
 'use strict';
+var html;
 
-function PopupCtrl($scope) {
+function PopupCtrl($scope, $http) {
   // TODO: スクレイピングしてデータを取得するように変更する
-  $http.get('url').success(data) {
-    console.log(data);
-  }
+  $http.get('http://terra.intra.tis.co.jp/aqua/atypes/userdir/').success(function(data) {
+  	html = $(data).find("div#node_item[indent='3'] div.section-body.stripe");
+  });
   $scope.members = [
     {name: 'てんてん', extNumber: '84-37750', status: '在席中'},
     {name: 'うがちゃん', extNumber: '84-11111', status: '離席中'},

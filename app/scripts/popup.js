79d81f6d8258b8cb21763109a8c2f34555bef1d5
tenten0213 <takehito.0213@gmail.com>
2014-06-09 12:48:22 +0900

    hide contents on error

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -4,17 +4,21 @@ var popup = angular.module('popup', ['ui.bootstrap']);
 
 popup.controller('PopupCtrl', ['$scope', '$http', '$filter', function($scope, $http, $filter) {
   $scope.predicate = 'mail';
+  $scope.alerts = [];
   $http.get('http://terra.intra.tis.co.jp/aqua/atypes/userdir/').success(function(data) {
-    var membersDom = $(data).find("div#node_item[indent='3'] div.section-body.stripe").children();
-    $scope.members = [];
-    $.each(membersDom, function(index, member) {
-      var memberData = $(member).find(".tab-text");
-      $scope.members.push({ name: $(memberData[0]).text().trim(), extNumber: $(memberData[2]).text()});
-    });
+    if ($(data).find("form[name=loginForm]").size() > 0) {
+      $scope.alerts.push({ type: 'warning', msg: 'Terraにログインしてください。' })
+    } else {
+      var membersDom = $(data).find("div#node_item[indent='3'] div.section-body.stripe").children();
+      $scope.members = [];
+      $.each(membersDom, function(index, member) {
+        var memberData = $(member).find(".tab-text");
+        $scope.members.push({ name: $(memberData[0]).text().trim(), extNumber: $(memberData[2]).text()});
+      });
+    }
   }).
   error(function(){
-    $scope.alerts = [];
-    $scope.alerts.push({ type: 'danger', msg: 'Terraにログインしてください。' })
+    $scope.alerts.push({ type: 'danger', msg: 'Terraにアクセスできません' })
   });
 
   $scope.addAlert = function() {

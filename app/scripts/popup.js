6a8066257cace3ea732a0cd121f408756470d8cf
tenten0213 <takehito.0213@gmail.com>
2014-06-09 13:07:29 +0900

    add filter by mail

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -3,7 +3,7 @@
 var popup = angular.module('popup', ['ui.bootstrap']);
 
 popup.controller('PopupCtrl', ['$scope', '$http', '$filter', function($scope, $http, $filter) {
-  $scope.predicate = 'mail';
+  $scope.predicate = 'mailAddress';
   $scope.alerts = [];
   $http.get('http://terra.intra.tis.co.jp/aqua/atypes/userdir/').success(function(data) {
     if ($(data).find("form[name=loginForm]").size() > 0) {
@@ -13,7 +13,7 @@ popup.controller('PopupCtrl', ['$scope', '$http', '$filter', function($scope, $h
       $scope.members = [];
       $.each(membersDom, function(index, member) {
         var memberData = $(member).find(".tab-text");
-        $scope.members.push({ name: $(memberData[0]).text().trim(), extNumber: $(memberData[2]).text()});
+        $scope.members.push({ initial: $(memberData[1]).text().charAt(0), name: $(memberData[0]).text().trim(), mailAddress: $(memberData[1]).text(), extNumber: $(memberData[2]).text()});
       });
     }
   }).

d29578bf748e4e0760431437a0bbd34c9271dd1c
tenten0213 <takehito.0213@gmail.com>
2014-06-13 19:42:39 +0900

    wip

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -7,13 +7,13 @@ options.controller('OptionsCtrl', ['$scope', '$http', '$localStorage', function(
   $scope.$storage = $localStorage;
 
   $scope.updateData = function() {
-    console.log("update");
     $http.get('http://terra.intra.tis.co.jp/aqua/atypes/userdir/view?exa=namelist-groupdir').success(function(data) {
       // 取得したページがログイン画面だったらアラートを表示
       if ($(data).find("form[name=loginForm]").size() > 0) {
         $scope.alerts.push({ type: 'warning', msg: 'Terraにログインしてください。' })
       } else {
         // DOMからデータを抽出してlocalstorageに保存する
+        $scope.$storage.members = [];
         var members = $(data).find("div#node_item div.section-body.stripe").children();
         $.each(members, function(index, member) {
           var memberDataList = $(member).find(".tab-text");
@@ -33,10 +33,9 @@ options.controller('OptionsCtrl', ['$scope', '$http', '$localStorage', function(
   $scope.addData = function() {
     $scope.$storage.members = $scope.$storage.members || [];
     $scope.$storage.members.push({ initial: "a", name: "tenten0213", mailAddress: "takehito.0213@gmail.com", extNumber: "84-12345"});
-    console.log($scope.$storage.members);
   }
 
-  // alertのclose用 ex: close="closeAlert($index)"
+  // alertのlcose用 ex: close="closeAlert($index)"
   $scope.closeAlert = function(index) {
     $scope.alerts.splice(index, 1);
   };
